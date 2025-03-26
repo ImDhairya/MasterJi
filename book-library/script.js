4b6feeb4cae5b9fa57af960e9697321424433cb5
ImDhairya <pandyadhairya789@gmail.com>
2025-03-26 08:36:26 +0530

    updates

--- book-library/script.js
+++ book-library/script.js
@@ -44,7 +44,39 @@ async function fetchData() {
 
 async function main() {
   await fetchData();
+  // console.log(collectedData);
+
+  const box = document.createElement("div");
   console.log(collectedData);
+
+  box.innerHTML = ` 
+  <div class="container">
+      <div class="box">
+        <div class="top">
+          <p class="id">${collectedData[0].id}</p>
+          <p class="title">${collectedData[0].volumeInfo.title}</p>
+        </div>
+        <div class="author">
+          <p class="people">by: ${collectedData[0].volumeInfo.authors}</p>
+          <div class="details">
+            <p>publishedDate: ${collectedData[0].volumeInfo.publishedDate}</p>
+            <p>publisher: ${collectedData[0].volumeInfo.publisher}</p>
+            <p>Pages: ${collectedData[0].volumeInfo.pageCount}</p>divdivdiv
+          </div>
+          <hr />
+          <p class="description">
+            can expect. Bottom line: This book will make you a better
+            developerBottom line: This book will make you a better
+            developerBottom line: This book will make you a better
+            developerBottom line: This book will make you a better developer.
+          </p>
+        </div>
+
+        <div class="bottom"></div>
+      </div>
+    </div`;
+
+  document.getElementById("container").appendChild(box);
 }
 
 main();
